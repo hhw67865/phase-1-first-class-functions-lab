ab11cb5013a0373ae805408548098b031acb55da
Henry Wu <hhw67865@gmail.com>
2022-10-11 18:23:37 -0400

    commit tree 222a0e28b46a130d45da006a3435f8ec91d12e45

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = array => {
+    const returnFirstTwoDrivers = [array[0], array[1]];
+    return returnFirstTwoDrivers;
+}
+
+const returnLastTwoDrivers = array => {
+    const returnLastTwoDrivers = [array[array.length-2], array[array.length-1]]
+    return returnLastTwoDrivers;
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(multiplier) {
+    return (fare) => multiplier*fare;
+}
+
+const fareDoubler = (fare) => createFareMultiplier(2)(fare);
+
+const fareTripler = (fare) => createFareMultiplier(3)(fare);
+
+function selectDifferentDrivers(array, callback) {
+    return callback(array);
+}
